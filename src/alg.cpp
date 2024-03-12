18fb771c234548afad209de81c7749a51293b8f9
momchik5 <160311115+momchik5@users.noreply.github.com>
2024-03-12 15:02:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,11 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(const int* arr, int size, int value) {
+  int quantity = 0;
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == value) {
+      quantity++;
+    }
+  }
+  return quantity;  // возвращает количество вхождений
 }
